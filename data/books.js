25968f475755e454b9cffea1a5bc847fec161408
K Miyano <miyano@miyacorata.net>
2020-04-09 11:04:19 +0900

    Add books data

--- data/books.js
+++ data/books.js
@@ -10,5 +10,100 @@ var books = [
         "author": "アサミンゴス",
         "seiyu": "今井麻美",
         "tweet_url": "https://twitter.com/asamingosu/status/1247868351830884352"
-    }
-]
+    },
+    {
+        "title": "春の詩集",
+        "author": "河井酔茗",
+        "seiyu": "園崎未恵",
+        "tweet_url": "https://twitter.com/Sonozaki_mie/status/1247873669889585152"
+    },
+    {
+        "title": "気絶人形",
+        "author": "原民喜",
+        "seiyu": "高橋李依",
+        "tweet_url": "https://twitter.com/taka8rie/status/1247803315989012481"
+    },
+    {
+        "title": "桃太郎",
+        "author": "楠山正雄",
+        "seiyu": "梶裕貴",
+        "tweet_url": "https://twitter.com/KAJI__OFFICIAL/status/1247856499927289859"
+    },
+    {
+        "title": "おやすみなさい、またあした",
+        "author": "井ノ上奈々",
+        "seiyu": "井ノ上奈々,市来光弘",
+        "tweet_url": "https://twitter.com/KAJI__OFFICIAL/status/1247856499927289859"
+    },
+    {
+        "title": "おやすみなさい、またあした",
+        "author": "井ノ上奈々",
+        "seiyu": "井ノ上奈々,市来光弘",
+        "tweet_url": "https://twitter.com/KAJI__OFFICIAL/status/1247856499927289859"
+    },
+    {
+        "title": "注文の多い料理店",
+        "author": "宮沢賢治",
+        "seiyu": "居留守かりん",
+        "tweet_url": "https://twitter.com/mosimosi_irusu/status/1247856825350774784"
+    },
+    {
+        "title": "よだかの星",
+        "author": "宮沢賢治",
+        "seiyu": "民安ともえ",
+        "tweet_url": "https://twitter.com/tammy_now/status/1247918302019502080"
+    },
+    {
+        "title": "彼岸花の教導",
+        "author": "片喰藤火",
+        "seiyu": "東北きりたん",
+        "tweet_url": "https://twitter.com/touka_katabami/status/1247902296601591808"
+    },
+    {
+        "title": "鬼護の石",
+        "author": "片喰藤火",
+        "seiyu": "東北きりたん",
+        "tweet_url": "https://twitter.com/touka_katabami/status/1247902202984681472"
+    },
+    {
+        "title": "自我ぜんまい",
+        "author": "片喰藤火",
+        "seiyu": "東北きりたん",
+        "tweet_url": "https://twitter.com/touka_katabami/status/1247902037179654145"
+    },
+    {
+        "title": "明日",
+        "author": "新美南吉",
+        "seiyu": "高倉裕貴",
+        "tweet_url": "https://twitter.com/takani_takakura/status/1247912946451148800"
+    },
+    {
+        "title": "うさぎとかえるの盆山",
+        "author": "河野佑紀",
+        "seiyu": "河野佑紀",
+        "tweet_url": "https://twitter.com/kawano_awano/status/1247856900143591425"
+    },
+    {
+        "title": "幸福が遠すぎたら",
+        "author": "寺山修司",
+        "seiyu": "新人声優",
+        "tweet_url": "https://twitter.com/ota__ayaka/status/1247944063573094401"
+    },
+    {
+        "title": "かくれんぼ",
+        "author": "村山籌子",
+        "seiyu": "琴音有波",
+        "tweet_url": "https://twitter.com/kotone_akv/status/1247934110430982146"
+    },
+];
+
+/* Template
+
+{
+    "title": "",
+    "author": "",
+    "seiyu": "",
+    "tweet_url": ""
+},
+
+*/
